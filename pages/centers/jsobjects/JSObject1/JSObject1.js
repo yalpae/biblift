395fda1f4621aea559ed2b9ce54b4922c5ab0ab8
Thabo Leroy Mketsu <mketsutl@gmail.com>
2026-01-09 13:13:25 +0000

    System generated commit, initial commit

---
+++ pages/centers/jsobjects/JSObject1/JSObject1.js
@@ -0,0 +1,23 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	var loc = navigator.geolocation;
+
+	function long() {
+		//	use async-await or promises
+		
+		if (loc) {
+		}
+		//	await storeValue('varName', 'hello world')
+	}
+
+	function lat() {
+		//	use async-await or promises
+		
+		if (loc) {
+			loc.
+		}
+		//	await storeValue('varName', 'hello world')
+	}
+
+}
